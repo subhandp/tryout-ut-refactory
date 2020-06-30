7814e964a9178f69549cbdb35d27771cf7933d04
subhan dinda putra <subhan.dinda.putra@gmail.com>
2020-06-30 20:24:24 +0800

    Update tradisional way reverse

--- 007.js
+++ 007.js
@@ -1,3 +1,15 @@
+//menggunakan for..loop
+function reverseForLoop(str) {
+    let revStr = "";
+ 
+    for (let s = str.length - 1; s >= 0; s--) { 
+        revStr += str[s];
+    }
+
+    return revStr; 
+}
+
+//menggunakan ES6
 function reverse(str) {
     const revStr = str.split('').reverse().join('');
     console.log(revStr);
