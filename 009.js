a62d3cbc4968a02e6b732a958084825542f0b52c
subhan dinda putra <subhan.dinda.putra@gmail.com>
2020-07-01 20:48:16 +0800

    update missing file

---
+++ 009.js
@@ -0,0 +1,5 @@
+let kota = ["Jakarta", "Aceh", "Malang", "Medan", "Bontang", "Jogja", "Jakarta", "Bandung", "Malang", "Solo", "Palembang", "Bandung"];
+
+let mySet = new Set(kota);
+
+console.log(mySet);
